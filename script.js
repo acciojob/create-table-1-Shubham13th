dadc1b7b7ae054d5798d7f1a88756c2a9330d565
Shubham Sharma <112186685+Shubham13th@users.noreply.github.com>
2025-09-01 17:39:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-function insert_Row() {
-    //Write your code here
-  
-  
-}
+ function insert_Row() {
+            //Write your code here
+            const table = document.getElementById("sampleTable");
+            const newRow = document.createElement("tr");
+            newRow.innerHTML = "<td>New cell1</td><td>New cell2</td>";
+            table.prepend(newRow);
+        }
